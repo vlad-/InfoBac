662abe0f23388fef2e345dbbe4c81fb1c45624d8
Furtuna Gabriel <gabi9107@yahoo.com>
2013-05-28 00:26:30 +0300

    clasa cu informatii despre ponderi , valoarea ponderii + alte valori pentru calcularea acesteia

---
+++ App_Code/WeightInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Weightinfo
+/// </summary>
+public class WeightInfo
+{
+    private double m_questionWeight;
+
+    public double QuestionWeight
+    {
+        get { return m_questionWeight; }
+    }
+
+    private int m_number;
+
+    public int Number
+    {
+        get { return m_number; }
+    }
+
+    private int m_mistakesNumber;
+
+    public int MistakesNumber
+    {
+        get { return m_mistakesNumber; }
+    }
+
+    private int m_streakNumber;
+
+    public int StreakNumber
+    {
+        get { return m_streakNumber; }
+    }
+
+    public WeightInfo(double weight, int number, int mistakesNumber, int streakNumber)
+    {
+        m_questionWeight = weight;
+        m_number = number;
+        m_mistakesNumber = mistakesNumber;
+        m_streakNumber = streakNumber;
+    }
+}
